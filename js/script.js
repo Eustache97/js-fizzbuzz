28b44a74badb1d08dccbc5ad62bf7a1ac94976ec
Eustache97 <eubomber97@gmail.com>
2022-10-10 18:03:13 +0200

    pseudo-codice

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// PSEUDO-CODICE
+//creo una variabile di appoggio
+//creo un loop inizializzando il contatore i a 1
+//finche il coltatore è minore uguale a 100 lo incremento di 1
+  // creo l'emento html
+  /*SE la divisione tra il valore del contatore e 3 mi da resto 0 E la divisione tra il valore
+   del contatore e 5 mi da resto 0*/
+    //ALLORA la variabile di appoggio conterrà FIZZBUZZ
+    /*vado ad inserire il contenuto della variabile d'appoggio nell'elemento html creato
+     aggiungendo la classe che mi da lo sfondo giallo-arancio*/
+  // ALTRIMENTI SE la divisione tra il valore del contatore e 3 mi da resto 0
+                  // ALLORA la variabile di appoggio conterrà FIZZ
+                  /*vado ad inserire il contenuto della variabile d'appoggio nell'elemento html creato
+                  aggiungendo la classe che mi da lo sfondo verde-acqua*/
+             // ALTRIMENTI SE la divisione tra il valore del contatore e 5 mi da resto 0
+                                //ALLORA la variabile di appoggio conterrà BUZZ 
+                            //ALTRIMENTI il valore della variabile d'appoggio sarà uguale al valore del contatore
+                            /*vado ad inserire il contenuto della variabile d'appoggio nell'elemento html creato
+                            aggiungendo la classe che mi da lo sfondo blu chiaro*/
+                                
+
+             
